8d7bb7355ac15dd0445eca257c8548816acad824
banditdev <thanwabandit.yos@gmail.com>
2025-06-09 20:49:54 +0700

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,30 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from 'cors'
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // What is an Endpoint?
 // An endpoint is a combintion of a URL + HTTP method
 // that lets the client interact with a specific resource.
 
 // middlewares
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
-
 
 // our simple custom middleware
 // app.use((req, res, next) => {
@@ -29,6 +35,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
